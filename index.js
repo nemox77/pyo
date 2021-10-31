d9f0f7fffc69f4fef2f9ba9e1622ab6648f35ace
Igor <igorkap@gmail.com>
2021-10-31 11:58:28 +0200

    Updating_node-js_code

--- index.js
+++ index.js
@@ -2,23 +2,36 @@
 // Node module: express-example-app
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
-
 'use strict';
 
 var express = require('express');
+var fs = require('fs');
 var count = 0;
 
 module.exports = function() {
-  var app = express();
-  
-  app.get('/', function(req, res) { 
-    res.json({result:'your request nuber is logged and count is:' + count});
-  });
+    var app = express();
 
-  app.post('/', function(req, res) {
-	 count++;
-    res.json({counter:count});
-  });
+    app.get('/', function(req, res) {
+        fs.readFile('counter.txt', 'utf8', function(err, data) {
+            if (err) throw err;
+            console.log(data);
+            res.json({
+                counter: data
+            });
+        });
 
-  return app;
+
+    });
+
+    app.post('/', function(req, res) {
+        count++;
+        res.json({
+            result: 'your request saved'
+        });
+        fs.writeFile('/data/counter.txt', count, function(err) {
+            if (err) return console.log(err);
+        });
+    });
+
+    return app;
 };
